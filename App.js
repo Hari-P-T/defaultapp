b9dea5299fa5a7f80e71eaa412b76b420006b626
Hari-P-T <112808592+Hari-P-T@users.noreply.github.com>
2023-10-18 13:01:47 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import RegistrationScreen from './RegistrationScreen';
+import LoginScreen from './LoginScreen'; // Make sure the path is correct
+
+const Stack = createStackNavigator();
+
+function App() {
+  // ... your navigation code ...
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Registration">
+        <Stack.Screen name="Registration" component={RegistrationScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
